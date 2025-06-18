5701b9d40362c220ec65e0f201401336c7070214
Gary Brown <GaryBrownDev@gmail.com>
2025-06-18 09:01:58 -0700

    Delayed hiding of window so B2S.Server can find the VPX window name and launch the back glass using the "find window by name" method

--- src/core/vpinball.cpp
+++ src/core/vpinball.cpp
@@ -1062,7 +1062,6 @@ void VPinball::DoPlay(const int playMode)
       return;
 
    PLOGI << "Starting Play mode [table: " << table->m_tableName << ", play mode: " << playMode << ']';
-   ShowWindow(SW_HIDE);
    bool initError = false;
    if (false)
    {
@@ -1226,6 +1225,8 @@ void VPinball::DoPlay(const int playMode)
       #else
       auto processWindowMessages = []() {};
       #endif
+      ShowWindow(SW_HIDE);
+
       g_pplayer->GameLoop(processWindowMessages);
 
       #if (defined(__APPLE__) && (defined(TARGET_OS_IOS) && TARGET_OS_IOS))
@@ -1235,9 +1236,9 @@ void VPinball::DoPlay(const int playMode)
 
       delete g_pplayer;
       g_pplayer = nullptr;
-   }
 
-   ShowWindow(SW_SHOW);
+     ShowWindow(SW_SHOW);
+   }
 
    if (initError)
    {
